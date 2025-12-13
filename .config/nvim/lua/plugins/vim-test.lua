0283dfaeddd8c10fbdeef236eaa5eb36bd2816ed
Dario Pranjic <derioreal63@gmail.com>
2025-12-13 22:15:09 +0100

    Add some stuff

--- .config/nvim/lua/plugins/vim-test.lua
+++ .config/nvim/lua/plugins/vim-test.lua
@@ -7,6 +7,7 @@ return {
     vim.keymap.set("n", "<leader>test", ":TestNearest<CR>", {})
     vim.keymap.set("n", "<leader>TT", ":TestFile<CR>", {})
     vim.keymap.set("n", "<leader>tall", ":TestSuite<CR>", {})
+    vim.keymap.set("n", "<leader>tlast", ":TestLast<CR>", {})
     vim.cmd("let test#strategy = 'vimux'")
   end,
 }
