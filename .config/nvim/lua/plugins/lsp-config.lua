5db9ec696d048de10886fdc548445a762929f35b
Dario Pranjic <derioreal63@gmail.com>
2026-03-06 12:03:07 +0100

    Change lsp and tmux

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -1,69 +1,86 @@
 return {
-  -- Mason: installs LSP servers
-  {
-    "williamboman/mason.nvim",
-    lazy = false,
-    config = function()
-      require("mason").setup()
-    end,
-  },
-
-  -- Mason-LSPConfig: bridges Mason + Neovim LSP
-  {
-    "williamboman/mason-lspconfig.nvim",
-    lazy = false,
-    opts = {
-      auto_install = true,
+    -- Mason: installs LSP servers
+    {
+        "williamboman/mason.nvim",
+        lazy = false,
+        config = function()
+            require("mason").setup()
+        end,
     },
-  },
 
-  -- Neovim's LSP client
-  {
-    "neovim/nvim-lspconfig",
-    lazy = false,
-    config = function()
-      -- Capabilities (required for nvim-cmp)
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+    -- Mason-LSPConfig: bridges Mason + Neovim LSP
+    {
+        "williamboman/mason-lspconfig.nvim",
+        lazy = false,
+        opts = {
+            auto_install = true,
+        },
+    },
+
+    -- Neovim's LSP client
+    {
+        "neovim/nvim-lspconfig",
+        lazy = false,
+        config = function()
+            -- Capabilities (required for nvim-cmp)
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            local settings = {
+                formatting = { enabled = true }
+            }
+
+            vim.lsp.config("ruby_lsp", {
+                capabilities = capabilities,
+                settings = settings,
+            })
+            vim.lsp.config("lua_ls", {
+                capabilities = capabilities,
+                settings = settings,
+            })
+            vim.lsp.config("ts_ls", {
+                capabilities = capabilities,
+                settings = settings,
+            })
+            vim.lsp.config("jsonls", {
+                capabilities = capabilities,
+                settings = settings,
+            })
+            vim.lsp.config("bufls", {
+                capabilities = capabilities,
+                settings = settings,
+            })
+            vim.lsp.config("buf_ls", {
+                capabilities = capabilities,
+                settings = settings,
+            })
 
 
-      ---------------------------------------------------------------------
-      -- Modern Neovim 0.11+ API
-      -- You define configs with vim.lsp.config(), then enable the server.
-      ---------------------------------------------------------------------
-
-      local settings = {
-        formatting = { enabled = true }
-      }
-
-      vim.lsp.config("ruby_lsp", {
-         capabilities = capabilities,
-         settings = settings,
-      })
-      vim.lsp.config("lua_ls", {
-         capabilities = capabilities,
-         settings = settings,
-      })
-      vim.lsp.config("ts_ls", {
-         capabilities = capabilities,
-         settings = settings,
-      })
-      vim.lsp.config("jsonls", {
-         capabilities = capabilities,
-         settings = settings,
-      })
-
-  -- LSP Keymaps
-      ---------------------------------------------------------------------
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-      vim.keymap.set("n", "<C-A-l>", function()
-        vim.lsp.buf.format({ async = false })
-      end, {})
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
-    end,
-  },
+            -- LSP Keymaps
+            ---------------------------------------------------------------------
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+            vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+            vim.keymap.set("n", "<C-A-l>", function()
+                vim.lsp.buf.format({ async = false })
+            end, {})
+            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+            vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+            vim.diagnostic.config({
+                virtual_text = {
+                    prefix = "●", -- symbol before message
+                    spacing = 4,
+                },
+                signs = true,
+                underline = true,
+                update_in_insert = false,
+                severity_sort = true,
+            })
+            vim.o.updatetime = 200
+            vim.api.nvim_create_autocmd("CursorHold", {
+                callback = function()
+                    vim.diagnostic.open_float(nil, { focus = false })
+                end
+            })
+        end,
+    },
 
 }
-
